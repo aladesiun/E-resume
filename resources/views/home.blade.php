83d5ffa963cc609ba711fbb14a0e0680e2376f06
Kennypapa <81877552+Kennypapa@users.noreply.github.com>
2021-12-26 14:43:53 -0800

    yur

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,15 @@
 <div>
     <div class="home_wrap">
        <div class="name_wrap">
-           <p class="nam_txt">Hello i'm</p>
-           <p class="nam_txt">Katy Brown</p>
+           <p class="nam_sec_txt"><span>poy stacy</span></p>
+           <div class="title_box">
+             <p class="title_txt">THis app will help you get your desired job.</p>
+           </div>
+           <div>
+               <button class="btn title_btn">
+                   Get started
+               </button>
+           </div>
        </div> 
     </div>
 </div>
